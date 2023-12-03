7fb4552474f7d6b0779f71603f061b529223af9e
Sibo <markxusibo@gmail.com>
2023-12-03 22:19:26 +0800

    setting up the project with creating all components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
-}
-
-)
-export class HeaderComponent{
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
 
 }
