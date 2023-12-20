121f700ee6c0899bbb3e8b7b03b99386fa1d1e4d
Sibo <markxusibo@gmail.com>
2023-12-20 21:16:20 +0800

    feat: add databinding to each components, now user can add shopping list and view each indiviual recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  loadedFeatured = 'recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeatured = feature;
+  }
+
 }
+
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,9 +8,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 
+  @Output() recipeWasSeclected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/1:1/w_3563,h_3563,c_limit/04102023-ratatouille-lede.jpg')
   ];
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSeclected.emit(recipe);
+  }
   
 }
